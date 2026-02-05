14f687eacddab28bab3b5942d879fdb0d9463e8b
gauravssah <124705806+gauravssah@users.noreply.github.com>
2026-02-05 17:33:19 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-import Footer from './components/Footer';
-import Header from './components/Header';
-import FindRightDoctor from './components/Homepages/FindRightDoctor';
-import HeroSection from './components/Homepages/HeroSection';
-import PopularDoctors from './components/Homepages/PopulerDoctors';
-import PromoCode from './components/Homepages/PromoCode';
-import Specialities from './components/Homepages/Specialities';
-import TotalNumbers from './components/Homepages/TotalNumbers';
-import WhyChooseUs from './components/Homepages/WhyChooseUs';
-import BookAppointment from './components/pages/BookAppointment'; // Cleaned path
-import Login from './components/auth/Login';
-import SignUp from './components/auth/SignUp';
-import ForgotPassword from './components/auth/ForgotPassword';
-import Treatement from './components/pages/Treatement';
-import FindDoctor from './components/pages/FindDoctor';
-import AskQuestions from './components/pages/AskQuestions';
-import VideoConsulting from './components/pages/VideoConsulting';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import FindRightDoctor from "./components/Homepages/FindRightDoctor";
+import HeroSection from "./components/Homepages/HeroSection";
+import PopularDoctors from "./components/Homepages/PopulerDoctors";
+import PromoCode from "./components/Homepages/PromoCode";
+import Specialities from "./components/Homepages/Specialities";
+import TotalNumbers from "./components/Homepages/TotalNumbers";
+import WhyChooseUs from "./components/Homepages/WhyChooseUs";
+import BookAppointment from "./components/pages/BookAppointment"; // Cleaned path
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import Treatement from "./components/pages/Treatement";
+import FindDoctor from "./components/pages/FindDoctor";
+import AskQuestions from "./components/pages/AskQuestions";
+import VideoConsulting from "./components/pages/VideoConsulting";
 
 function App() {
   return (
@@ -25,7 +25,6 @@ function App() {
       <Header />
 
       <Routes>
-        {/* Home Page */}
         <Route
           path="/"
           element={
@@ -41,14 +40,11 @@ function App() {
           }
         />
 
-        {/* Book Appointment Page */}
         <Route path="/book-appointment" element={<BookAppointment />} />
         <Route path="/treatments" element={<Treatement />} />
         <Route path="/ask-question" element={<AskQuestions />} />
         <Route path="/find-doctor" element={<FindDoctor />} />
         <Route path="/video-consulting" element={<VideoConsulting />} />
-
-        {/* Authentication Pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
